82b5d9d9d31b64f3dcf183348af78f05e7e023d2
penwei wang <115126898+penweiwang1@users.noreply.github.com>
2023-02-05 08:46:06 -0500

    Add files via upload

---
+++ MechaCarChallenge.RScript.R
@@ -0,0 +1,4 @@
+library(tidyverse)
+mechacar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar))
